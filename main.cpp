291aad204e99d9491b2adafaed165d983b5229c2
SaReyesGonzalez <sa.reyesgonzalez@gmail.com>
2023-12-18 22:29:57 -0300

    Creacion Menu de la aplicación

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <fstream>
 #include <sstream>
+#include <limits>
 #include "NodoServidores.h"
 #include "AristaConexiones.h"
 #include "Grafo.h"
@@ -10,16 +11,71 @@ using namespace std;
 
 vector<NodoServidores*> lecturaServidores();
 vector<AristaConexiones*> lecturaConexiones();
+bool existeUsuarioId(int user, vector<NodoServidores*> vServidores);
+int registro(bool flag, int userId, const vector<NodoServidores*>& vServidores);
 
 int main() {
 
     vector<NodoServidores*> vServidores = lecturaServidores();
     vector<AristaConexiones*> vConexiones = lecturaConexiones();
-
     auto* grafo = new Grafo(vServidores, vConexiones);
 
+    char funcion;
+    int userId = 0;
+    bool flag = true;
 
+    cout << "Bienvenido a: Simulador - aplicacion de mensajeria !" << endl;
 
+    //Registro
+    userId = registro(flag,userId,vServidores);
+
+    // opciones menu
+    do {
+        cout << "\n----- PANEL DE OPCIONES -----" << endl;
+        cout << "A) Calcular tiempo de demora entre usted y un destino." << endl; //Funcion basica
+        cout << "B) Calcular tiempo de demora entre un origen y destino." << endl; //Funcion extra 1
+        cout << "C) Funcion extra 2" << endl;
+        cout << "D) Funcion extra 3" << endl;
+        cout << "E) Salir" << endl;
+
+        flag = true;
+
+        cout << "\nIngrese la funcion que desea usar: ";
+        cin >> funcion;
+
+        switch (funcion) {
+
+            case 'a':
+            case 'A':
+                cout << "a" << endl;
+                break;
+
+            case 'b':
+            case 'B':
+                cout << "b" << endl;
+                break;
+
+            case 'c':
+            case 'C':
+                cout << "c" << endl;
+                break;
+
+            case 'd':
+            case 'D':
+                cout << "d" << endl;
+                break;
+
+            case 'e':
+            case 'E':
+                cout << "\nHasta luego, vuelva pronto." << endl;
+                flag = false;
+                break;
+
+            default:
+                cout << "Opcion invalida. Pruebe nuevamente." << endl;
+        }
+
+    } while(flag);
 
     return 0;
 }
@@ -91,4 +147,34 @@ vector<AristaConexiones*> lecturaConexiones() {
         cerr << "Error al abrir archivo CSV." << endl;
         return {};
     }
+}
+
+bool existeUsuarioId(int userId, vector<NodoServidores*> vServidores) {
+
+    for (int i = 0 ; i < vServidores.size() ; i++) {
+        if (userId == vServidores.at(i)->getId()) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+int registro(bool flag, int userId, const vector<NodoServidores*>& vServidores) {
+    do {
+        flag = true;
+
+        cout << "Ingrese su id de usuario: ";
+        cin >> userId;
+
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        flag = existeUsuarioId(userId,vServidores);
+
+        if (!flag) {
+            cout << "Id usuario no existe, ingrese un id nuevamente." << endl;
+        }
+
+    } while (!flag);
+
+    return userId;
 }
