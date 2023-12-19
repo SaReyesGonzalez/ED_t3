1d5fc49841b8c24c05376291d316018cf814f7dc
SaReyesGonzalez <sa.reyesgonzalez@gmail.com>
2023-12-19 02:02:24 -0300

    lectura arreglada

---
+++ cmake-build-debug/CMakeFiles/ED_t3.dir/AristaConexiones.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/ED_t3.dir/AristaConexiones.cpp.o: \
+ /home/sebastian/Programacion/C++/ED_t3/AristaConexiones.cpp \
+ /usr/include/stdc-predef.h \
+ /home/sebastian/Programacion/C++/ED_t3/AristaConexiones.h
